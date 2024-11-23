69a20ccf7ccb4332c91a0a288125a573f5eff90f
Jayson Virissimo <jayson.virissimo@asu.edu>
2024-11-23 15:35:53 -0700

    Move config into JSON file

--- spec/commentary_spec.rb
+++ spec/commentary_spec.rb
@@ -1,5 +1,17 @@
+require 'json'
 require 'rexml/document'
 require 'rspec'
+
+RSpec.describe "Commentary JSON file" do
+  let(:file_path) { File.join(File.dirname(__FILE__), '..', 'commentary.json') }
+
+  it "is valid JSON" do
+    expect {
+      file_content = File.read(file_path)
+      JSON.parse(file_content)
+    }.not_to raise_error
+  end
+end
 
 RSpec.describe "Commentary RSS feed" do
   let(:file_path) { File.join(File.dirname(__FILE__), '..', 'commentary.rss') }
